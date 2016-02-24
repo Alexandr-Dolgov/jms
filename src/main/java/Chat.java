7dc9a1b484995f360a846cd368f687bc64d1921a
alexd <alexd@z-inc.net>
2016-02-24 12:20:29 +0500

    начато добавление очередей

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -4,30 +4,34 @@ import javax.naming.*;
 
 public class Chat implements javax.jms.MessageListener {
 
+    private String username;
+    private long quantityMessages;  //msgid
+
     private TopicSession topicPublisherSession;
     private TopicPublisher topicPublisher;
     private TopicConnection topicConnection;
-    private String username;
 
-    private long quantityMessages;
+    private QueueConnection queueConnection;
+
 
     /* Constructor used to Initialize Chat */
-    public Chat(String username)
-            throws Exception {
+    public Chat(String username) throws Exception {
 
         // Obtain a JNDI topicConnection using the jndi.properties file
-        InitialContext ctx = new InitialContext();
+        Context context = new InitialContext();
+
+        //--------------------топик-------------------------
 
         // Look up a JMS topicConnection factory and create the topicConnection
-        TopicConnectionFactory conFactory = (TopicConnectionFactory) ctx.lookup("TopicCF");
-        TopicConnection topicConnection = conFactory.createTopicConnection();
+        TopicConnectionFactory topicCF = (TopicConnectionFactory) context.lookup("TopicCF");
+        TopicConnection topicConnection = topicCF.createTopicConnection();
 
         // Create two JMS session objects
         TopicSession topicPublisherSession = topicConnection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
         TopicSession topicSubscriberSession = topicConnection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
 
         // Look up a JMS topic
-        Topic chatTopic = (Topic) ctx.lookup("ALL");
+        Topic chatTopic = (Topic) context.lookup("ALL");
 
         // Create a JMS topicPublisher and topicSubscriber. The additional parameters
         // on the createSubscriber are a message selector (null) and a true
@@ -47,6 +51,19 @@ public class Chat implements javax.jms.MessageListener {
 
         // Start the JMS topicConnection; allows messages to be delivered
         topicConnection.start();
+
+        //-------------------очередь------------------------
+
+        QueueConnectionFactory queueCF = (QueueConnectionFactory) context.lookup("QueueCF");
+        QueueConnection queueConnection = queueCF.createQueueConnection();
+        QueueSession queueSession = queueConnection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Queue requestQ = queueSession.createQueue(username);
+
+        this.queueConnection = queueConnection;
+
+        queueConnection.start();
+
     }
 
     /* Receive Messages From Topic Subscriber */
@@ -60,16 +77,31 @@ public class Chat implements javax.jms.MessageListener {
     }
 
     /* Create and Send Message Using Publisher */
-    protected void writeMessage(String text) throws JMSException {
+    private void writeMessage(String text) throws JMSException {
         quantityMessages++;
+
+        if (text.startsWith("ALL ")) {
+            writeMessageInTopicAll(text);
+        } else {
+            String receiverName = text.substring(0, text.indexOf(' '));
+            writeMessageInQueue(receiverName, text);
+        }
+    }
+
+    private void writeMessageInTopicAll(String text) throws JMSException {
         TextMessage message = topicPublisherSession.createTextMessage();
         message.setText(username + ": " + text);
         topicPublisher.publish(message);
     }
 
+    private void writeMessageInQueue(String queue, String text) {
+        System.out.println("отправка сообдещия '" + text + "' в очередь '" + queue + "'");
+    }
+
     /* Close the JMS Connection */
     public void close() throws JMSException {
         topicConnection.close();
+        queueConnection.close();
     }
 
     /* Run the Chat Client */
@@ -79,8 +111,10 @@ public class Chat implements javax.jms.MessageListener {
                 System.out.println("username missing");
             }
 
-            // args[0]=topicFactory; args[1]=username
-            Chat chat = new Chat(args[0]);
+            String username = args[0];
+            Chat chat = new Chat(username);
+
+            System.out.println("Chat " + username + " started");
 
             // Read from command line
             BufferedReader commandLine = new BufferedReader(new InputStreamReader(System.in));
